ebc530dd870ee54d0632232d2ef6864a2950eb4f
Sandoval015 <ynbolanoss@udistrital.edu.co>
2026-03-16 09:10:19 -0500

    primer tipo de codigo cmd.py

--- cmd.py
+++ cmd.py
@@ -1,1 +1,14 @@
 //en este archivo va el programa
+productos = []
+precios = []
+
+def registrar_producto():
+    nombre = input("Ingrese el nombre del producto: ")
+    precio = float(input("Ingrese el precio del producto: "))
+    
+    productos.append(nombre)
+    precios.append(precio)
+    
+    print("Producto registrado correctamente\n")
+
+
