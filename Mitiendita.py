ff5b3d855b1534072fef5c3f1fd4196c21c5795a
BrandonUU379 <bsurriagou@udistrital.edu.co>
2026-04-06 02:22:35 -0500

    reincertando el codigo y ordenando

---
+++ Mitiendita.py
@@ -0,0 +1,24 @@
+
+productos = []
+precios = []
+
+def registrar_producto():
+    nombre = input("Ingrese el nombre del producto: ")
+    precio = float(input("Ingrese el precio del producto: "))
+    
+    productos.append(nombre)
+    precios.append(precio)
+    
+    print("Producto registrado correctamente\n")
+
+def menu():
+    while True:
+        print("===== TIENDA ESCOLAR =====")
+        print("1. Registrar producto")
+        print("2. Mostrar productos")
+        print("3. Calcular total de compra")
+        print("4. Salir")
+        
+    print("Producto registrado correctamente\n")
+    
+     
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# MiTiendita
+<!-- # MiTiendita -->
 Este aplicativo es una herramienta de consola diseñada para facilitar la administración básica de una tienda escolar. Su objetivo es permitir a los usuarios registrar productos y gestionar compras de forma rápida y sencilla a través de una interfaz de texto.
